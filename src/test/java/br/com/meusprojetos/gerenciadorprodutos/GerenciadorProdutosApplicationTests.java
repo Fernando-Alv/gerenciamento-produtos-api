3b5d7d55935542bf76d66cf735f1c90311d5de2e
Fernando-Alv <fernando.alv.tei@hotmail.com>
2025-03-22 15:03:33 -0300

    Initial commit

---
+++ src/test/java/br/com/meusprojetos/gerenciadorprodutos/GerenciadorProdutosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.meusprojetos.gerenciadorprodutos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorProdutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
